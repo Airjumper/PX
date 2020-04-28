3ba7923d0ef1c0c5a0e64bac6f7650b99439714a
Yansong <18817456@student.westernsydney.edu.au>
2020-04-28 07:28:56 +0000

    update

--- runserver.py
+++ runserver.py
@@ -2,36 +2,15 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
-from flask import render_template
-from PX_web import app
+from os import environ
+from flask import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
